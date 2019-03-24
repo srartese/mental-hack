c3f747213c6e9b2e6e76523254fbd651bcf860ec
Sara Artese <sara.artese@heliosinteractive.com>
2019-03-23 21:12:27 -0700

    moved firebase init

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
 import store from './store';
-import firebase form 'firebase';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
 
+const config = {
+    apiKey: "AIzaSyBky0ko6qX3eaI0dcV844tW00EF3LQrZGI",
+    authDomain: "mental-hack.firebaseapp.com",
+    databaseURL: "https://mental-hack.firebaseio.com",
+    projectId: "mental-hack",
+    storageBucket: "mental-hack.appspot.com",
+    messagingSenderId: "629474644428"
+};
+
+firebase.initializeApp(config);
 let app;
 firebase.auth().onAuthStateChanged(user => {
 	if(!app) {
@@ -13,9 +23,8 @@ firebase.auth().onAuthStateChanged(user => {
 			el: '#app',
       store,
       router,
+      user,
       render: h => h(App)
 		});
 	}
 });
-
-    /* eslint-disable no-new */
